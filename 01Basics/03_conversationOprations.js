217deadd9d73784ea8ce9cd7a02a9c7de85ed56f
Mohammad Asif <mathstrick45@gmail.com>
2026-08-04 06:15:36 +0000

    oprations

---
+++ 01Basics/03_conversationOprations.js
@@ -0,0 +1,18 @@
+let num = 3;
+let negNum = -num;
+
+// console.log(negNum);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(3%2);
+
+// console.log( "1" + 3)
+// console.log( "1" + "3")
+// console.log( 1 + "3")
+// console.log( 1 + 1 + "3")
+// console.log( 1 + "3" + "2")
+// console.log( 1 + "3" + 2 + 2 )
+// console.log( 2 + 1 + "3" + "2")
