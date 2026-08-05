a7f622ba46ddf404f466043e5105d9a558b79bd1
Mohammad Asif <mathstrick45@gmail.com>
2026-08-05 16:23:43 +0000

    datatypes_summary

---
+++ 01Basics/summary.js
@@ -0,0 +1,29 @@
+// DATA TYPES
+// data rtypes are two types
+// 1. primitive data types
+// 2. Non Primitive(Referance)data types
+
+// PRIMITIVE DATA TYPES
+// primitive data types are 7 type
+//string,number,boolean,null,undefined,symbol,bigInt;
+
+const name = 123
+const score = 231
+const isSignin = true
+const isTemprature = null
+let future;
+const myFriendsName = Symbol('Asif')
+const myName = Symbol('Asif')
+const bigInt = 12345678901234165456431544321n
+
+console.table({name,score,isSignin,isTemprature,future,myFriendsName,myName,bigInt});
+
+
+//NOT PRIMITIVE DATA TYPES
+// array,functions,objects
+
+
+const myFnctions = function(){
+    name = "asif"
+    age = 20
+}
