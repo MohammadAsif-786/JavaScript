c470c9e38692290436044926375ac5e0f64af85c
Mohammad Asif <mathstrick45@gmail.com>
2026-08-09 06:44:08 +0000

    strings

---
+++ 01Basics/05_strings.js
@@ -0,0 +1,41 @@
+const myName = "Asif"
+const age = 20
+
+// console.log (myName + age);
+
+// console.log (`my name is ${myName} and my age is ${age}`);
+
+const gameName = new String('Free fire')
+
+// console.log(gameName[2]);
+// console.log (gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf(' e'));
+
+// console.log(gameName.split(` `));
+
+// console.log(gameName.substring(0,3).toUpperCase());
+
+// const newGame = gameName.slice(-6,0);
+// console.log (newGame);
+
+// const game = "     pubg    "
+// console.log(game.trim());
+// console.log(game);
+// console.log(gameName.slice(-2,2));
+
+// const id = "abc1edf1ghi1"
+// console.log(id.replace(`1`,  "-"));
+
+console.log(gameName.split(` `))
+console.log(gameName.at(-2))
+
+const id = [1,2,3,4,5];
+console.log(id);
+const newwId = id.shift();
+console.log(newwId);
+console.log(id);
