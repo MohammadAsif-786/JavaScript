e90ac399a424ebf1afa301c388bbe2962d44fca2
Mohammad Asif <mathstrick45@gmail.com>
2026-08-10 17:46:51 +0000

    numbers

---
+++ 01Basics/06_num_and_math.js
@@ -0,0 +1,26 @@
+const score = 400
+
+console.log(score);
+
+const number = new Number(1000);
+
+console.log(number);
+
+// console.log(number.toString());
+// console.log(number.toString().length);
+// console.log(number.toFixed(3));
+console.log(number.toPrecision(2));
+
+const number1 = 10000000
+console.log(number1.toLocaleString(`en-IN`));
+
+
+// +++++++++++++++MATHS++++++++++++++
+
+console.log(Math)
+
+console.log(Math.round(2.3))
+console.log(Math.floor(2.3))
+console.log(Math.ceil(2.3))
+console.log(Math.min(2,4,6,3, 6,));
+console.log(Math.max(2,4,6,3,8,56,));
