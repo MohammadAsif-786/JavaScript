b8e4e071f8d8ef793681b4e3a3bff64ba94d4170
Mohammad Asif <mathstrick45@gmail.com>
2026-08-12 14:49:39 +0000

    date & time

---
+++ 01Basics/07_dateinJs.js
@@ -0,0 +1,24 @@
+let myDate = new Date()
+
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(typeof(myDate));
+
+
+// let newmyDate = new Date(2025, 7,11,17,50)
+// console.log(newmyDate.toString());
+// let newmyDate = new Date("2026-12-26")
+// console.log(newmyDate.toLocaleString());
+
+let dateStamp = new Date()
+
+// console.log(Math.floor((dateStamp)/1000));
+// console.log((newmyDate.getTime())/1000);
+
+console.log(dateStamp.getDay());
+console.log(dateStamp.getMonth() + 1);
+console.log(dateStamp.getSeconds());
+console.log(dateStamp.getHours());
