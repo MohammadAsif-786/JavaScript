6cbed76dcf5faecb9817ee422e57541590e92815
Mohammad Asif <mathstrick45@gmail.com>
2026-08-12 13:54:20 +0000

    num&math

--- 01Basics/06_num_and_math.js
+++ 01Basics/06_num_and_math.js
@@ -2,14 +2,14 @@ const score = 400
 
 console.log(score);
 
-const number = new Number(1000);
+const number = new Number(10000);
 
-console.log(number);
+// console.log(number);
 
 // console.log(number.toString());
 // console.log(number.toString().length);
 // console.log(number.toFixed(3));
-console.log(number.toPrecision(2));
+console.log(number.toPrecision(3));
 
 const number1 = 10000000
 console.log(number1.toLocaleString(`en-IN`));
@@ -17,10 +17,17 @@ console.log(number1.toLocaleString(`en-IN`));
 
 // +++++++++++++++MATHS++++++++++++++
 
-console.log(Math)
+// console.log(Math)
 
-console.log(Math.round(2.3))
-console.log(Math.floor(2.3))
-console.log(Math.ceil(2.3))
-console.log(Math.min(2,4,6,3, 6,));
-console.log(Math.max(2,4,6,3,8,56,));
+// console.log(Math.round(2.3))
+// console.log(Math.floor(2.3))
+// console.log(Math.ceil(2.3))
+// console.log(Math.min(2,4,6,3, 6,));
+// console.log(Math.max(2,4,6,3,8,56,));
+// console.log((Math.random())*10+1);
+
+const min = 10
+const max = 25
+
+console.log(Math.floor(Math.random() * (max - min + 1)+ min));
+
