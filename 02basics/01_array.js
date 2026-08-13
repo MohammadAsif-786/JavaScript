e0ee8d19a84b2f77e8bb027d1be84ed4a596b59e
Mohammad Asif <mathstrick45@gmail.com>
2026-08-13 18:31:08 +0000

    arrays

---
+++ 02basics/01_array.js
@@ -0,0 +1,35 @@
+const arr1 = [1,2,3,4,5,6]
+const arr2 = ["asif","aman","zaid"]
+const arr3 = new Array(7,8,9,0);
+console.log(arr1);
+
+
+// console.log(arr1);
+// console.log(arr2);
+// console.log(arr3);
+
+
+// methopd of arrays
+
+// arr1.push(9); //output= [1,2,3,4,5,6,9]
+// arr2.pop()
+// console.log(arr1);
+// console.log(arr2);
+
+// arr1.shift()
+// arr1.unshift(8)
+// console.log(arr1);
+
+
+// const arrn = arr1.indexOf(2);
+// console.log(arr1.includes(5));
+// console.log(arrn);
+// const arrS = arr1.slice(1,4);
+// const arrP = arr1.splice(1,4);
+// console.log(arrS);
+// console.log(arr1);
+// const arrT = arr1.join();
+// console.log(arrT);
+const arrS = arr1.splice(1,3)
+console.log(arrS);
+console.log(typeof(arr1[2]));
