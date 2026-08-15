8722fbeb8cf6baa932f4a144432c52d4dcf5cd95
Mohammad Asif <mathstrick45@gmail.com>
2026-08-15 05:29:22 +0000

    02_array

---
+++ 02basics/02_array.js
@@ -0,0 +1,30 @@
+const hero = ["IronMan","Thor","C.America"]
+const realHero = ['Khan SIR',"Alakh SIR","Razwan SIR"]
+const marval = [1,2,3,4]
+
+// console.log(hero);
+// console.log(realHero);
+// hero.push(realHero);
+// console.log(hero[3][2]);
+
+// const allHeros = hero.concat(realHero)
+// console.log(allHeros);
+
+// const heros = [...hero ,...realHero,...marval]
+
+// const more_array = [1,2,3,[4,5,6,[7,8,9]],[1,2,3,4]]
+
+// console.log(more_array);
+// const flat_array = more_array.flat(Infinity)
+// console.log(flat_array);
+
+console.log(Array.from("Asif"));
+console.log(Array.isArray("Asif"));
+console.log(Array.from({name: "Asif"})); // intresting OutPut 
+
+const score1 = 100
+const score2 = 600
+const score3 = 300
+const score4 = 500
+
+console.log(Array.of(score1,score2,score3,score4,));
