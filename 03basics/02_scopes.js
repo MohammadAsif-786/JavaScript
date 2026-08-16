b918b632b144723471208748b3725961b83cc480
Mohammad Asif <mathstrick45@gmail.com>
2026-08-16 13:56:28 +0000

    scopes

---
+++ 03basics/02_scopes.js
@@ -0,0 +1,51 @@
+// if(true){
+//     let a = 1
+//     const b = 2
+//     // var c = 3
+//     console.log(a);
+
+// }
+// let a = 12
+// // c = 13
+// // console.log(a);
+// // console.log(b);
+// console.log(a); 
+
+// function one(){
+//     const userName="Asif"
+//     function two(){
+//         name="Sansif"
+//         console.log(userName);
+//         console.log(name);
+//     }
+//     // console.log(name);
+//     // two()
+//     // console.log(userName);
+// }
+
+// one()
+if(true){
+    const myName = "Asif"
+    if(myName==="Asif"){
+        collage = " GCRG Group"
+        // console.log(`My name is ${myName} and my collage name is ${collage}`);
+        // console.log(myName + collage);
+    }
+    console.log(collage);
+}
+// copnsole.log(myName);
+
+// ++++++++++++++++++++++ interesting ++++++++++++++++++++
+ nav1(2)
+function nav1(num1) {
+    return num1 + 2
+   
+
+}
+// console.log(nav1(3));
+// nav2(4)
+ const nav2 = function nav(num2){
+    return num2 + 5
+ }
+
+ 
