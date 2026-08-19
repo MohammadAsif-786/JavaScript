b93cd3e477b33a75df5dda8f019762ee7e86312f
Mohammad Asif <mathstrick45@gmail.com>
2026-08-19 15:28:21 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,20 @@
+// For
+
+// for (let i  = 0; i < 11; i++) {
+//     const element = i;
+//     if(element == 8){
+//         console.log("8 is lucky number")
+//     }
+//     console.log(element);
+// }
+
+// console.log(element);
+for (let i = 1; i < 5; i++) {
+    // console.log(i);
+    for (let j = 1; j < 5; j++) {
+        // console.log(`iner loop runs ${j} and outer loop run ${i} `);
+        console.log(j*i);
+    }
+        
+    
+}
