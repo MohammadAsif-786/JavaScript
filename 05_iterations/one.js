1ef061c265fbeaa41c694cfc8227ea491dad246f
Mohammad Asif <mathstrick45@gmail.com>
2026-08-20 17:15:17 +0000

    Loops

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -8,13 +8,38 @@
 //     console.log(element);
 // }
 
-// console.log(element);
-for (let i = 1; i < 5; i++) {
-    // console.log(i);
-    for (let j = 1; j < 5; j++) {
-        // console.log(`iner loop runs ${j} and outer loop run ${i} `);
-        console.log(j*i);
-    }
+// // console.log(element);
+// for (let i = 1; i < 5; i++) {
+//     // console.log(i);
+//     for (let j = 1; j < 5; j++) {
+//         // console.log(`iner loop runs ${j} and outer loop run ${i} `);
+//         console.log(`${i}*${j} = `+ i*j  );
+//     }
         
     
-}
+// // }
+// let myArray = ["Asif", "Aman", "zaid","Mumtaj"]
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+// }
+
+// Breack and continue
+
+// for (let index = 1; index <= 20; index++) {
+//     if(index == 8){
+//         console.log(`Best Value of I = ${index}`);
+//         break
+//     }
+//     console.log(`Value of i is ${index}`);
+    
+// }  
+for (let index = 1; index <= 20; index++) {
+    if(index == 8){
+        console.log(`Best Value of I = ${index}`);
+        continue
+    }
+    console.log(`Value of i is ${index}`);
+    
+}  
