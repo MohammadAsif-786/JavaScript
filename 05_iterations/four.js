b9b66da0bee7d31f3e228f5fb070ef47ca4f86a5
Mohammad Asif <mathstrick45@gmail.com>
2026-08-22 17:53:46 +0000

    forin loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,17 @@
+const myObject = {
+    js: "java script",
+    coa: "computer organigation arcitactur"
+}
+
+for (const key  in myObject){
+
+//    console.log(`${key} full form is ${myObject[key]}`);
+    
+}
+
+const subject = [ "coa","dstl","math 4","ds"]
+
+for (const key in subject) {
+    // console.log(subject[key]);   
+}
+
